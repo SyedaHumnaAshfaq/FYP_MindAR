e441e788abeacb029515d5a2e196d48c84070f6f
Syeda Humna Ashfaq <shumnaashfaq@gmail.com>
2024-06-28 19:37:48 +0500

    mern implemented
    added mern stack folder structure and ran it on localhost to check

---
+++ public/scripting.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const list = ["earring", "earring1","earring2","earring3","nosepin","glasses"];
+    const visibles = [false, true];
+    const setVisible = (button, entities, visible) => {
+        if (visible) {
+            button.classList.add("selected");
+        } else {
+            button.classList.remove("selected");
+        }
+        entities.forEach((entity) => {
+            entity.setAttribute("visible", visible);
+        });
+    }
+    list.forEach((item, index) => {
+        const button = document.querySelector("#" + item);
+        const entities = document.querySelectorAll("." + item + "-entity");
+        setVisible(button, entities, visibles[index]);
+        button.addEventListener('click', () => {
+            visibles[index] = !visibles[index];
+            setVisible(button, entities, visibles[index]);
+        });
+    });
+});
+
+
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use('/models', express.static(path.join(__dirname, 'models')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+// Set the path to your views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Define route to render the homepage
+app.get('/', (req, res) => {
+  res.render('pages/HomePage'); // Render the homepage.ejs file in the pages folder
+});
+app.get('/VirtualTryOn', (req, res) => { 
+    res.render('pages/VirtualTryOn'); // Render the about.ejs file in the pages folder
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
