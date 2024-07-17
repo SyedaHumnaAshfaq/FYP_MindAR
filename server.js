50155eb2955c44f03bdeacd82021fac258205357
Syeda Humna Ashfaq <shumnaashfaq@gmail.com>
2024-07-17 19:40:42 +0500

    mongoDB connected

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
+const connectDB = require('./db');
 
+connectDB();
 app.set('view engine', 'ejs');
 app.use('/models', express.static(path.join(__dirname, 'models')));
 
