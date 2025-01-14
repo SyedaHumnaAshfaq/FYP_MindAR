842f04feb6e517c88a6b66daf5682e9a4e92f696
Ayesha Rao <126611837+Ayesha-Rao@users.noreply.github.com>
2025-01-14 14:46:44 +0500

    video

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 const connectDB = require('./db');
 const userRoutes = require('./routes/userRoutes');
